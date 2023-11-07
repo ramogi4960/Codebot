edf8674e1c93fd9633a9792ad7fd8a63659043bf
Kevin <kramzyvine@icloud.com>
2023-11-07 19:43:36 +0300

    added prism and bootstrap to home.html with context functionality

--- website/views.py
+++ website/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
 
-def homepage(request):
-    return render(request, "homepage.html")
+def home(request):
+    language_list = "html+markup+css+clike+javascript+c+csharp+cpp+css+python+r"
+    language_list = sorted(language_list.split("+"))
+
+    context = {
+        "languages": language_list,
+    }
+
+    if request.method == "POST":
+        code = request.POST['code']
+        lang = request.POST['lang']
+
+        context = {
+            "languages": language_list,
+            "lang": lang,
+            "code": code,
+        }
+
+    return render(request, "home.html", context)
